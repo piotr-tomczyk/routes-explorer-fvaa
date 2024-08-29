85a36ffb7c64c64d760fea3b2d653d768953fe25
Piotr Tomczyk <travis.blonded@gmail.com>
2024-08-29 23:43:10 +0200

    Add ts-node for running scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
+	build: {
+		manifest: true,
+		rollupOptions: {
+			external: [
+				'src/scripts/scheduleConverter.ts',
+			],
+		},
+	},
 });
